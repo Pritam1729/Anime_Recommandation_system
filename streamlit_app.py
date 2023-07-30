5357207c2a7bbdfdb7bdd140c6aca2ce9f31fd05
Pritam1729 <106548018+Pritam1729@users.noreply.github.com>
2023-07-30 16:51:20 +0530

    after remove streamlit mark

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,14 @@ from test import show_page
 
 st.set_page_config(layout="wide")
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 
 show_page()
